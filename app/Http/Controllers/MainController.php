f54d454316aef031a6a9721ffbbfdaf29b48a2d0
igatenru <igatenru@gmail.com>
2022-10-27 17:54:02 +0300

    Second part: view & controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,8 +7,11 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
-        return view('welcome');
+        $query = $request->all();
+        //dd($query);
+        $imgsrc = 'https://cdn2.hexlet.io/assets/logo_ru-495f05850e0095ea722a2b583565d492719579c02b0ce61d924e4f895fabf781.svg';
+        return view('welcome', compact('query', 'imgsrc'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,14 @@
         <title>{{ env('APP_NAME') }}</title>
     </head>
     <body>
-        
+        <h1>Добро пожаловать в {{ env('APP_NAME') }}!</h1>
+        <img width='300px' src='{{ $imgsrc }}'>
+        <ul>
+        @forelse ($query as $q => $v) 
+            <li>{{ $q }} = {{ $v }}</li>
+        @empty
+            <p>No data in $query</p>
+        @endforelse
+        </ul>
     </body>
 </html>
